91ac51e8ac2a381d912044e712a803828e3327fc
pujasharma5431 <pujasharma5431@gmail.com>
2023-01-04 15:41:32 +0545

    strings

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+//        Variables
+        String name= "Puja";
+        int age=23;
+        String name1= "Arjun";
+        int age1 = 25;
+
+        String name2=name1;
+        System.out.println(name + name2 + name1);
+
+    }
+}
