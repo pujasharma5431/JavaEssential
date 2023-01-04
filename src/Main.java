7b3cda8906a29f6acf38428b865240ccd3f66210
pujasharma5431 <pujasharma5431@gmail.com>
2023-01-04 17:25:51 +0545

    strings

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
 //        Variables
+
+
         String name= "Puja";
         int age=23;
         String name1= "Arjun";
@@ -9,6 +11,45 @@ public class Main {
 
         String name2=name1;
         System.out.println(name + name2 + name1);
+
+
+//        Primitive types
+        byte age2 =30;
+        int phone =1234567899;
+long phone2 = 123479466934L;
+float pi = 3.13F;
+char letter = '@';
+boolean isAdult = false;
+
+//Non-primitive types
+
+        String name3= "Arjun";
+        System.out.println(name3.length());
+
+//        Non-primitive types
+
+        String name6 =new String("Poo");
+
+
+
+// Strings
+        String naame ="Arjun";
+        String naame2 ="Puja";
+        String naame3= naame + " and "+ naame2;
+        System.out.println(naame3);
+
+
+//        charAt
+        
+
+
+
+
+
+
+
+
+
 
     }
 }
