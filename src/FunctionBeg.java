7762067fa9449be6692d8602f9b4d5f0a24b897a
pujasharma5431 <pujasharma5431@gmail.com>
2023-01-09 11:02:23 +0545

    Break and Continue

---
+++ src/FunctionBeg.java
@@ -0,0 +1,12 @@
+public class FunctionBeg {
+    public static void printJava(){
+        System.out.println("Heyy Java");
+    }
+    public static void FunctionBeg(String[] args){
+//METHODS
+        printJava();
+        printJava();
+        printJava();
+
+    }
+}
